3b4879218fa77890481dde6b59b20f68b3e8c6c5
futa4095 <kick20000@gmail.com>
2021-06-27 18:55:31 +0900

    コマンド初回作成

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # bgm-recommend
+
+Helping you find today's background music
+
+## Installation
+
+```sh
+npm install bgm-recommend
+```
+
+## Usage
+
+Set the API Key to an environment variable and execute the command.
+
+```sh
+export BGM_CANDIDATES_API_KEY=YOUR_API_KEY
+npx bgm-recommend
+```
+
+I will provide you with three search terms, and you can choose any one of them.
+
+```sh
+? Pick the one you like … 
+❯ piano music
+  Bossa Nova Live
+  celtic 24/7
+```
+
+Recommend (up to three) YouTube videos to use as background music.
+
+```sh
+✔ Pick the one you like · celtic 24/7
+These look good.
+🎶🛡️ Celtic and Viking Music 24/7 (Celtic &amp; Viking Music Radio) 🛡️
+🔗https://www.youtube.com/watch?v=W5a2F3lHVb8
+🎶Boston Celtics center Enes Kanter wins 24/7 Title: Raw Exclusive, Sept. 9, 2019
+🔗https://www.youtube.com/watch?v=C7RTlHl3kR0
+🎶Medieval Castle Music | Folk, Traditional, Fantasy | 24/7 Live Stream (Ean Grimm)
+🔗https://www.youtube.com/watch?v=roEuYLuOw2w
+```
---
+++ index.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+
+const search = require('youtube-search')
+const { Select } = require('enquirer')
+const ENV_NAME_API_KEY = 'BGM_CANDIDATES_API_KEY'
+
+const chooseKeyword = async () => {
+  const keywordList = () => {
+    const genreList = ['jazz', 'relax', 'study', 'piano', 'Bossa Nova', 'hawaiian', 'celtic']
+      .sort(() => (Math.random() - 0.5))
+      .slice(0, 3)
+    const suffixList = ['music', 'radio', '24/7', 'Live', '作業用BGM']
+      .sort(() => (Math.random() - 0.5))
+      .slice(0, 3)
+    return genreList.map((genre, i) => {
+      return [genre, suffixList[i]].join(' ')
+    })
+  }
+  const prompt = new Select({
+    name: 'choose keyword',
+    message: 'Pick the one you like',
+    choices: keywordList()
+  })
+  return prompt.run()
+}
+
+const searchBgm = async keyword => {
+  const opts = {
+    type: ['video'],
+    maxResults: 3,
+    key: process.env[ENV_NAME_API_KEY]
+  }
+  return new Promise((resolve, reject) => {
+    search(keyword, opts, function (err, results) {
+      if (err) {
+        reject(err)
+        return
+      }
+      const candidates = results.map(result => [result.title, result.link])
+      resolve(candidates)
+    })
+  })
+}
+
+const display = recommends => {
+  console.log('These look good.')
+  recommends.forEach(element => {
+    console.log('🎶' + element[0])
+    console.log('🔗' + element[1])
+  })
+}
+
+const main = async () => {
+  if (!process.env[ENV_NAME_API_KEY]) {
+    console.log(`Environment variable ${ENV_NAME_API_KEY} is missing`)
+    process.exit(1)
+  }
+
+  try {
+    const keyword = await chooseKeyword()
+    const recommends = await searchBgm(keyword)
+    display(recommends)
+  } catch (err) {
+    console.error(err.message)
+    process.exitCode = 1
+  }
+}
+
+main()
